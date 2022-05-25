7c7b166af727b630017588c41ade69f6980296d5
ikenwaa <ajohn9353@gmail.com>
2022-05-25 18:16:19 +0100

    added Connect Wallet button

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,46 @@
 import React, { useMemo } from "react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {ConnectionProvider, WalletProvider, walletProvider} from "@solana/wallet-adapter-react";
+import {
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import "@solana/wallet-adapter-react-ui/styles.css";
 import "../styles/globals.css";
 import "../styles/App.css";
 
 const App = ({ Component, pageProps }) => {
 
+  // Set network to 'mainnet-beta', 'testnet' or 'devnet'
+  const network = WalletAdapterNetwork.Devnet;
+
+  //Set custom RPC endpoint
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  // Setup wallets
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+    ], [network]
+  );
+
   return (
-    <Component {...pageProps} />
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,39 @@
 import React from "react";
+import { PublicKey } from "@solana/web3.js";
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import HeadComponent from '../components/Head';
 
 // Constants
-const TWITTER_HANDLE = "_buildspace";
+const TWITTER_HANDLE = "im_ikenwa";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  
+  // Fetch the user's public key (wallet address) from any supported wallet
+  const { publicKey } = useWallet();
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://media.giphy.com/media/fY6aIWIkX0EB4vUyXm/giphy.gif" alt="emoji" width="480"/>
+
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>
+    </div>
+  )
   
   return (
     <div className="App">
       <HeadComponent/>
       <div className="container">
         <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
+          <p className="header">👟 Welcome to Augustine's Sneakerz Store 👟</p>
+          <p className="sub-text">The only sneakerz shop that accepts sh*tcoins for dope sh*t... No CAP 👌</p>
         </header>
 
         <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+          {/** Render the "Connected" button if publicKey exists */}
+          {publicKey ? 'Connected!' : renderNotConnectedContainer()}
         </main>
 
         <div className="footer-container">
@@ -28,7 +43,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`Built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
